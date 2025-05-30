a8587618dce81122360ec6b0d81d32f3d16528b4
Christopher Tydings <ctydings@vt.edu>
2025-05-30 04:39:19 -0800

    Renamed CASS to cass

---
+++ app/src/main/java/cass/data/BaseDTO.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cass.data;
+
+/**
+ *
+ * @author ctydi
+ */
+public class BaseDTO {
+
+    private int key;
+
+    public BaseDTO(int key) {
+        this.key = key;
+    }
+
+    public BaseDTO() {
+
+    }
+
+    public void setKey(int toSet) {
+        this.key = toSet;
+    }
+
+    public int getKey() {
+        return this.key;
+    }
+
+}
