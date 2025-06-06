6fe4dfc9eeb5cdb9a35ca37319095139b415c0d2
ctydings-dev <84865284+ctydings-dev@users.noreply.github.com>
2025-06-05 21:01:35 -0400

    Nightly Commit.

---
+++ app/src/main/java/CASS/data/item/ItemDTO.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CASS.data.item;
+
+import CASS.data.BaseDTO;
+import CASS.data.CreatedDTO;
+
+/**
+ *
+ * @author ctydi
+ */
+public class ItemDTO extends CreatedDTO{
+    
+ 
+     
+     private String itemName;
+             
+             private String itemAlias;
+             private Integer itemType;
+             
+             private boolean isForSale;
+             private Integer company;
+             
+             
+    
+    
+    
+    public ItemDTO(Integer key){
+        super(key);
+    }
+
+    
+    public ItemDTO(Integer key, String name, String alias, Integer type, Integer company, boolean isForSale){
+        super(key);
+        this.itemName = name;
+        this.itemAlias = alias;
+        this.itemType = type;
+        this.company = company;
+        this.isForSale = isForSale == true;
+        
+    }
+    
+    
+    
+    public boolean isForSale(){
+        return this.isForSale == true;
+    }
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getItemAlias() {
+        return itemAlias;
+    }
+
+    public Integer getItemType() {
+        return itemType;
+    }
+
+    public Integer getCompany() {
+        return company;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
