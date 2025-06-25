4da77c83baf37ab5378a50d09001c8d0e63f425c
Christopher Tydings <ctydings@vt.edu>
2025-06-25 13:36:27 -0800

    Added the service provider.

--- app/src/main/java/CASS/services/ServiceProvider.java
+++ app/src/main/java/CASS/services/ServiceProvider.java
@@ -4,14 +4,54 @@
  */
 package CASS.services;
 
+import CASS.services.mysql.MySqlService;
+import CASS.services.mysql.SqlAddressService;
+import CASS.services.mysql.SqlInvoiceService;
+import CASS.services.mysql.SqlItemService;
+import CASS.services.mysql.SqlPersonService;
+import CASS.services.mysql.SqlTypeService;
+import java.sql.SQLException;
+
 /**
  *
  * @author ctydi
  */
 public class ServiceProvider {
 
-    public static AddressService getAddressService() {
-        return null;
+    private static String user = "";
+
+    private static String password = "";
+
+    private static String server = "localhost";
+
+    private static int port = 3306;
+
+    private static String database = "";
+
+    public static AddressService getAddressService() throws SQLException {
+        return new SqlAddressService(getMySql());
+    }
+
+    public static PersonService getPersonService() throws SQLException {
+        return new SqlPersonService(getMySql());
+    }
+
+    public static ItemService getItemService() throws SQLException {
+        return new SqlItemService(getMySql());
+    }
+
+    public static TypeService getTypeService() throws SQLException {
+        return new SqlTypeService(getMySql());
+    }
+
+    public static InvoiceService getInvoiceService() throws SQLException {
+        return new SqlInvoiceService(getMySql(), getItemService());
+    }
+
+    public static MySqlService getMySql() throws SQLException {
+
+        return new MySqlService(user, password, server, "" + port, database);
+
     }
 
 }
